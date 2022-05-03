5a7da11817b4da700fae37c592e616a610e80935
FieryXJoe <josephmsherry@comcast.net>
2022-05-03 02:01:49 -0400

    Now shows all items

--- midterm/src/Items.js
+++ midterm/src/Items.js
@@ -1,9 +1,11 @@
 import { Navigate,useNavigate } from 'react-router-dom';
+import FetchingAllData from './FetchingAllData';
 
 function Items() {
   return (
-      <>
-      </>
+      <div className="items">
+          <FetchingAllData />
+      </div>
   );
 }
 
