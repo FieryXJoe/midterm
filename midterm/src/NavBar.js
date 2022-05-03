09216e4cdd555c254832b4fee051f1ef1052776e
FieryXJoe <josephmsherry@comcast.net>
2022-05-02 21:50:50 -0400

    Basic content for NavBar

--- midterm/src/NavBar.js
+++ midterm/src/NavBar.js
@@ -1,9 +1,15 @@
+import { Navigate,useNavigate } from 'react-router-dom';
 import './App.css';
 
 function NavBar() {
   return (
     <div className="App">
-
+        <button onClick = {() => navigate('home')}>
+            Home
+        </button>
+        <button onClick = {() => navigate('items')}>
+            Items
+        </button>
     </div>
   );
 }
