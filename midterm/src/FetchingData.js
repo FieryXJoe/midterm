a7c6d00e15f7a47a1f62bbf89ee5d92d85cbc9b0
FieryXJoe <josephmsherry@comcast.net>
2022-05-03 03:09:14 -0400

    Styling for item page

--- midterm/src/FetchingData.js
+++ midterm/src/FetchingData.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import useFetchID from './hooks/useFetchID';
 import Loading from './Loading';
 
@@ -6,6 +7,7 @@ const FetchingData = (props) => {
     const[posts, setPosts] = useState([]);
 
     const {data, loading, error} = useFetchID(`products`, props.number);
+    const navigate = useNavigate();
 
     if(loading){
         return  <Loading />;
@@ -14,12 +16,16 @@ const FetchingData = (props) => {
         { data.length && data.map((data) => {
             return(
                 <>
-
-                    {data.title},
-                    {data.price},
-                    {data.description},
-                    {data.category},
-                    {data.image}
+                    <img src = {data.image}></img>
+                    <div className='details-div'>
+                        <h3>{data.category}</h3>
+                        <h1>{data.title}</h1>
+                        <h2>{data.price}</h2>                    
+                        <h4>{data.description}</h4>
+                        <button onClick = {() => navigate('/items')} className="button">
+                            Items
+                        </button>
+                    </div>
                 </>    
         )})}  
         </>);}
