d14441f36b7e655a05ef48d2e22de78e332ac906
FieryXJoe <josephmsherry@comcast.net>
2022-05-02 21:33:42 -0400

    Set Up React Router Basics

--- midterm/src/index.js
+++ midterm/src/index.js
@@ -3,11 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        {/* The Nav Bar */}
+        <Route path="/" element={}>
+          {/* The Home Page */}
+          <Route index element={}/>
+          {/* The Items */}
+          <Route path='state-demo' element={} />
+          {/* Item Display Page */}
+          <Route path='effect-demo' element={}>
+            {/* Item Index # */}
+            <Route path=':startingCount' element={} />
+          </Route>
+          {/* The Home Page */}
+          <Route path='*' element={} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
