d6dc808e31d93c2e893b45ec8a3527b4958512ea
FieryXJoe <josephmsherry@comcast.net>
2022-05-06 20:04:49 -0400

    Now has capability to take a number parameter and return only one entry

--- midterm/src/hooks/useFetch.js
+++ midterm/src/hooks/useFetch.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const baseURL = 'http://localhost:3000/'
 
-const useFetch = (endURL) => {
+const useFetch = (endURL, num) => {
     const[loading, setLoading] = useState(true);
     const[data, setData] = useState([]);
     const[error, setError] = useState('');
@@ -11,7 +11,7 @@ const useFetch = (endURL) => {
     useEffect(() => {
         const getData = async () => {
             try{
-                const response = await axios.get(`${baseURL}${endURL}`);
+                const response = await axios.get(`${baseURL}${endURL}`, {params:{id:num}});
                 setData(response.data);
             }catch (error){
                 setError(error);
