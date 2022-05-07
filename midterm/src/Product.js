17cb4c3d528e2b8e0b25dfa39ea389148c7a9a4c
FieryXJoe <josephmsherry@comcast.net>
2022-05-06 20:17:11 -0400

    Removed loop, refer to data by array index now. Also removed unused code/imports

--- midterm/src/Product.js
+++ midterm/src/Product.js
@@ -1,33 +1,27 @@
-import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import useFetch from './hooks/useFetch';
 import Loading from './Loading';
 
 const Product = (props) => {
-    const[posts, setPosts] = useState([]);
-
-    const {data, loading, error} = useFetch(`products`, props.number);
+    const { data, loading, error } = useFetch(`products`, props.number);
     const navigate = useNavigate();
 
-    if(loading){
-        return  <Loading />;
+    if (loading) {
+        return <Loading />;
     }
     return (<>
-        { data.length && data.map((data) => {
-            return(
-                <>
-                    <img src = {data.image}></img>
-                    <div className='details-div'>
-                        <h3>{data.category}</h3>
-                        <h1>{data.title}</h1>
-                        <h2>{data.price}</h2>                    
-                        <h4>{data.description}</h4>
-                        <button onClick = {() => navigate('/items')} className="button">
-                            Items
-                        </button>
-                    </div>
-                </>    
-        )})}  
-        </>);}
-        
+        <img src={data[0].image}></img>
+        <div className='details-div'>
+            <h3>{data[0].category}</h3>
+            <h1>{data[0].title}</h1>
+            <h2>{data[0].price}</h2>
+            <h4>{data[0].description}</h4>
+            <button onClick={() => navigate('/items')} className="button">
+                Items
+            </button>
+        </div>
+    </>
+    );
+}
+
 export default Product;
