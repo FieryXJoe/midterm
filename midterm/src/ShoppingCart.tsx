5da580eeeba099c39e954f518264feeaa7d9adad
FieryXJoe <josephmsherry@comcast.net>
2022-05-31 15:17:27 -0400

    Added Modal popup to shopping cart

--- midterm/src/ShoppingCart.tsx
+++ midterm/src/ShoppingCart.tsx
@@ -1,39 +1,66 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Box from './components/Box';
 import useFetchWholeCart from './hooks/useFetchWholeCart';
 import Loading from './Loading';
 
-function ShoppingCart () {
+function ShoppingCart() {
     const navigate = useNavigate();
 
-    const {data, loading, error} = useFetchWholeCart('cart');
+    const { data, loading, error } = useFetchWholeCart('cart');
+    const [show, setShow] = useState(false);
 
-    if(loading){
-        return  <Loading />;
+    if (loading) {
+        return <Loading />;
     }
     let total = 0;
-    return (<table className="shopping-cart-table"><tbody>
-        <tr>
-            <td>Item</td>
-            <td>Price Per Item</td>
-            <td>Quantity</td>
-            <td>Total Cost</td>
-        </tr>
-        { data.length && data.map((data) => {
-            total += data.price * data.quantity
-            return(
-                    <tr key ={data.title}>
+    if (show) {
+        return (
+            <div>
+                <div className="checkout">
+                    Would you like to confirm the purchase?
+                    <br />
+                    <button onClick={() => setShow(false)} className="button">
+                        Confirm
+                    </button>
+                    <button onClick={() => setShow(false)} className="button">
+                        Cancel
+                    </button>
+                </div>
+            </div>
+        );
+    }
+    else {
+        return (<><table className="shopping-cart-table"><tbody>
+            <tr>
+                <td>Item</td>
+                <td>Price Per Item</td>
+                <td>Quantity</td>
+                <td>Total Cost</td>
+            </tr>
+            {data.length && data.map((data) => {
+                total += data.price * data.quantity
+                return (
+                    <tr key={data.title}>
                         <td>{data.title}</td>
                         <td>{data.price}</td>
                         <td>{data.quantity}</td>
                         <td>{data.price * data.quantity}</td>
-                    </tr>  
-        )})}  
-        <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td>{total}</td>
-        </tr>
-        </tbody></table>);}
-        
+                    </tr>
+                )
+            })}
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>{total}</td>
+            </tr>
+        </tbody></table>
+            <button onClick={() => setShow(true)} className="button">
+                Checkout
+            </button>
+        </>);
+    }
+}
+
 export default ShoppingCart;
